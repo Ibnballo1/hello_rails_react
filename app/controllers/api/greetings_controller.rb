757771470c8dad25f7d4fbac6422869cbf3f8506
Ibnballo1 <belloabdullateef035@gmail.com>
2023-04-05 07:08:59 +0100

    Update controller and route

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,2 +1,6 @@
 class Api::GreetingsController < ApplicationController
+  def random
+    @greeting = Greeting.order("RANDOM()").first
+    render json: @greeting
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    get 'greetings/random', to: 'greetings#random'
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'greetings#index'
 end

881aa9037e14fd1c153139f4e99504f70e5962e5
Ibnballo1 <belloabdullateef035@gmail.com>
2023-04-05 13:08:57 +0100

    Update store

--- app/javascript/store.js
+++ app/javascript/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { reducer as greetingReducer } from './features/greetingSlice';
+import greetingReducer from "./features/greetingSlice";
 
 export default configureStore({
   reducer: {

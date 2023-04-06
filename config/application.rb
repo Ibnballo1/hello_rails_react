e8a7b2a85ce246d148f04a2a14ec5b5d7ea2f5ce
Ibnballo1 <belloabdullateef035@gmail.com>
2023-04-06 15:06:27 +0100

    Fix application.js error

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module HelloRailsReact
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.paths << Rails.root.join("app/javascript")
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
